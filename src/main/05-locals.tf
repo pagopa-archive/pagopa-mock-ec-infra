89353b77c284b01b0f9b22ed7a7209aca5d04b90
uolter <walter.traspadini@pagopa.it>
2023-01-20 14:48:42 +0100

    removed ecs fargate

--- src/main/05-locals.tf
+++ src/main/05-locals.tf
@@ -1,13 +1,3 @@
 locals {
   project = format("%s-%s", var.app_name, var.env_short)
-
-  ecs_cluster_name = format("%s-ecs-cluster", local.project)
-  ecs_task_name    = format("%s-mockec-task", local.project)
-
-  mockec_container_port = 80
-
-  logs = {
-    mock = "/ecs/mockec"
-  }
-
 }
--- src/main/98-variables.tf
+++ src/main/98-variables.tf
@@ -22,49 +22,6 @@ variable "env_short" {
   description = "Evnironment short."
 }
 
-variable "vpc_cidr" {
-  type        = string
-  default     = "172.16.128.0/18"
-  description = "VPC cidr."
-}
-
-variable "azs" {
-  type        = list(string)
-  description = "Availability zones"
-  default     = ["eu-south-1a", "eu-south-1b", "eu-south-1c"]
-}
-
-variable "vpc_private_subnets_cidr" {
-  type        = list(string)
-  description = "Private subnets list of cidr."
-  default     = ["172.16.128.0/24", "172.16.129.0/24", "172.16.130.0/24"]
-}
-
-variable "vpc_public_subnets_cidr" {
-  type        = list(string)
-  description = "Private subnets list of cidr."
-  default     = ["172.16.131.0/24", "172.16.132.0/24", "172.16.133.0/24"]
-}
-
-variable "vpc_internal_subnets_cidr" {
-  type        = list(string)
-  description = "Internal subnets list of cidr. Mainly for private endpoints"
-  default     = []
-}
-
-variable "enable_nat_gateway" {
-  type        = bool
-  description = "Enable/Create nat gateway"
-  default     = true
-}
-
-## ECS
-variable "logs_tasks_retention" {
-  type        = number
-  description = "Log task retantion (days)."
-  default     = 7
-}
-
 variable "tags" {
   type = map(any)
   default = {
